743533f63e59e34fb8503928fc95a47f4b2ebfdd
Aleksandr Sokolovskii <github@amsokol.io>
2019-01-12 17:09:03 +0300

    added inline doc

--- flutter_client/lib/app.dart
+++ flutter_client/lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'chat_screen.dart';
 
+/// FriendlychatApp is Flutter application
 class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
