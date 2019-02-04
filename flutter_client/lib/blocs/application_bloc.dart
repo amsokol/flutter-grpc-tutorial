2016733622e458600643759ba2ab85f479d57775
Aleksandr Sokolovskii <github@amsokol.io>
2019-02-04 19:06:12 +0300

    added inline documentation

--- flutter_client/lib/blocs/application_bloc.dart
+++ flutter_client/lib/blocs/application_bloc.dart
@@ -9,36 +9,45 @@ import 'package:flutter_client/models/message_outgoing.dart';
 import 'bloc_provider.dart';
 import 'message_events.dart';
 
+/// BLoC class
 class ApplicationBloc implements BlocBase {
+  // Application state (chat messages)
   final _messages = Set<Message>();
 
+  /// Controller is used to notify when message created
   final _messageCreatedController =
       new BehaviorSubject<MessageNewCreatedEvent>();
   Sink<MessageNewCreatedEvent> get inNewMessageCreated =>
       _messageCreatedController.sink;
 
+  /// Controller is used to send message to the server
   final _messageSendController = new BehaviorSubject<MessageNewCreatedEvent>();
   Sink<MessageNewCreatedEvent> get inMessageSend => _messageSendController.sink;
   Stream<MessageNewCreatedEvent> get outMessageSend =>
       _messageSendController.stream;
 
+  /// Controller is used to notify when message sent to the server
   final _messageSentController = new BehaviorSubject<MessageSentEvent>();
   Sink<MessageSentEvent> get inMessageSent => _messageSentController.sink;
 
+  /// Controller is used to notify when message failed to send to the server
   final _messageSendFailedController =
       new BehaviorSubject<MessageSendFailedEvent>();
   Sink<MessageSendFailedEvent> get inMessageSendFailed =>
       _messageSendFailedController.sink;
 
+  /// Controller is used to notify when message received from the server
   final _messageReceivedController =
       new BehaviorSubject<MessageReceivedEvent>();
   Sink<MessageReceivedEvent> get inMessageReceived =>
       _messageReceivedController.sink;
 
+  /// Controller is used to provide state (chat messages) to the widgets
   final _messagesController = new BehaviorSubject<List<Message>>(seedValue: []);
   Sink<List<Message>> get _inMessages => _messagesController.sink;
   Stream<List<Message>> get outMessages => _messagesController.stream;
 
+  /// Constructor
   ApplicationBloc() {
     _messageCreatedController.listen(_onNewMessageCreated);
     _messageSentController.listen(_onMessageSent);
@@ -46,6 +55,7 @@ class ApplicationBloc implements BlocBase {
     _messageReceivedController.listen(_onMessageReceived);
   }
 
+  /// Dispose resources
   void dispose() {
     _messageCreatedController.close();
     _messageSendController.close();
@@ -55,31 +65,37 @@ class ApplicationBloc implements BlocBase {
     _messagesController.close();
   }
 
+  /// Handle event: new message created
   void _onNewMessageCreated(MessageNewCreatedEvent event) {
     _messages.add(event.message);
     _notify();
     _messageSendController.add(event);
   }
 
+  /// Handle event: message sent to the server
   void _onMessageSent(MessageSentEvent event) {
     _findOutgoingMessage(event.id).status = MessageOutgoingStatus.SENT;
     _notify();
   }
 
+  /// Handle event: message failed to send to the server
   void _onMessageSendFailed(MessageSendFailedEvent event) {
     _findOutgoingMessage(event.id).status = MessageOutgoingStatus.FAILED;
     _notify();
   }
 
+  /// Handle event: message received from the server
   void _onMessageReceived(MessageReceivedEvent event) {
     _messages.add(MessageIncoming(text: event.text));
     _notify();
   }
 
+  /// Publish state (chat messages) to the widgets
   void _notify() {
     _inMessages.add(UnmodifiableListView(_messages));
   }
 
+  /// Find outgoing message by 'id' in the state (chat messages)
   MessageOutgoing _findOutgoingMessage(String id) {
     var message =
         _messages.firstWhere((message) => message.id == id, orElse: () => null);
--- flutter_client/lib/blocs/message_events.dart
+++ flutter_client/lib/blocs/message_events.dart
@@ -2,18 +2,21 @@ import 'package:meta/meta.dart';
 
 import 'package:flutter_client/models/message_outgoing.dart';
 
+/// New message created event
 class MessageNewCreatedEvent {
   final MessageOutgoing message;
 
   MessageNewCreatedEvent({@required this.message});
 }
 
+/// Message sent to the server event
 class MessageSentEvent {
   final String id;
 
   MessageSentEvent({@required this.id});
 }
 
+/// Message failed to send to the server event
 class MessageSendFailedEvent {
   final String id;
   final String error;
@@ -21,12 +24,14 @@ class MessageSendFailedEvent {
   MessageSendFailedEvent({@required this.id, @required this.error});
 }
 
+/// Message received from the server event
 class MessageReceivedEvent {
   final String text;
 
   MessageReceivedEvent({@required this.text});
 }
 
+/// Message failed to receive from the server event
 class MessageReceiveFailedEvent {
   final String error;
 
